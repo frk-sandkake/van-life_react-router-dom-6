8bb14b81ddd5c553525b6b52dd65c80c6d56eb28
Linda Sandaker <frk.sandkake@gmail.com>
2023-06-22 23:57:43 +0200

    - header, Link + <Route></Route>, import Layout

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LayoutMain from './components/LayoutMain'
 import Home from './pages/Home'
 import About from './pages/About'
 import Vans from './pages/vans/Vans'
@@ -12,18 +13,13 @@ import "./server"
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className='site-logo' to='/'>#VanLife</Link>
-        <nav>
-          <Link to={'/about'}>About</Link>
-          <Link to={'/vans'}>Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/vans' element={<Vans />}/>
-        <Route path='/vans/:id' element={<VanDetails />}/>
+        <Route element={<LayoutMain />}>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/vans' element={<Vans />}/>
+          <Route path='/vans/:id' element={<VanDetails />}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
