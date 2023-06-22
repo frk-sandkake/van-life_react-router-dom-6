de4a1e96c17890c2c132ca3aa1a696b9cfb1c747
Linda Sandaker <frk.sandkake@gmail.com>
2023-06-23 01:42:38 +0200

    + container, main & Footer

--- src/components/LayoutMain.jsx
+++ src/components/LayoutMain.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 import { Outlet } from 'react-router-dom'
 import Header from "./Header"
+import Footer from "./Footer"
 
 export default function LayoutMain() {
     return (
-        <>
+        <div className="site-wrapper">
             <Header />
+            <main>
             <Outlet />
-        </>
+            </main>
+            <Footer />
+        </div>
     )
 }
