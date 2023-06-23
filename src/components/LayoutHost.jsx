c70b620b9716abf9dbc2811743ff4b5ccd0d1b7f
Linda Sandaker <frk.sandkake@gmail.com>
2023-06-24 01:07:10 +0200

    activeStyle on NavLinks

--- src/components/LayoutHost.jsx
+++ src/components/LayoutHost.jsx
@@ -15,26 +15,22 @@ export default function LayoutHost() {
                 <NavLink
                     to='.'
                     end
-                    style={({ isActive }) => isActive ? activeStyles : null}
-                >
+                    style={({ isActive }) => isActive ? activeStyles : null}>
                     Dashboard
                 </NavLink>
                 <NavLink
                     to='income'
-                    style={({ isActive }) => isActive ? activeStyles : null}
-                >
+                    style={({ isActive }) => isActive ? activeStyles : null}>
                     Income
                 </NavLink>
                 <NavLink
                     to="vans"
-                    style={({ isActive }) => isActive ? activeStyles : null}
-                >
+                    style={({ isActive }) => isActive ? activeStyles : null}>
                     Vans
                 </NavLink>
                 <NavLink
                     to='reviews'
-                    style={({ isActive }) => isActive ? activeStyles : null}
-                >
+                    style={({ isActive }) => isActive ? activeStyles : null}>
                     Reviews
                 </NavLink>
 
